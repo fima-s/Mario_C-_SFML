6737ce9a8c5fe788d8c2e5cbcb3d76cdea0bf80b
fima-s <shevrin.f@gmail.com>
2018-03-22 01:24:41 +0300

    uddate_/FORCE:MULTIPLE

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "class_Q_Block.h"
 #include "class_Coin.h"
 #include "class_Flag.h"
-#include "Map.h"
+//#include "Map.h"
 
 
 #include <string>
@@ -19,6 +19,10 @@
 
 float offsetX = 0;
 float offsetY = 0;
+
+
+void _world_generation(std::list<Abstract*> & Dynamic_obj_list, sf::Texture & map_set, sf::Texture & coins_tex);
+
 
 
 
@@ -80,101 +84,8 @@ int main()
 	std::list<Abstract*> Dynamic_obj_list;
 
 
-	//////////////////////////////// Enemies //////////////////////////////////////////////////
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 10 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 15 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 22 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 30 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 50 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 56 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 75 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 78 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 96 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 115 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 128 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 135 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 168 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 180 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
-	Dynamic_obj_list.push_back(new Enemy("Mushroom", 190 * 16, 18 * 16, map_set, 17, 0, 18, 16, 2));
 
-
-	//////////////////////////////// Coins //////////////////////////////////////////////////
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 13 * 16, 13 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 14 * 16, 13 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 15 * 16, 13 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 41 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 42 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 43 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 50 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 51 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 52 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 53 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 52 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 53 * 16, 10 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 84 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 85 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 86 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 95 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 96 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 137 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 139 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 148 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 149 * 16, 11 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 184 * 16, 14 * 16, coins_tex, 147, 98, 9, 15, 4));
-	Dynamic_obj_list.push_back(new Coin("Coin_classic", 186 * 16, 14 * 16, coins_tex, 147, 98, 9, 15, 4));
-
-
-	for (int i = 0; i < 7; ++i)
-	{
-		Dynamic_obj_list.push_back(new Coin("Coin_classic", (30 + i) * 16, 13 * 16, coins_tex, 147, 98, 9, 15, 4));
-	}
-	
-
-
-
-	// Jumping coins
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 12 * 16, 14 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 12 * 16, 14 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 14 * 16, 10 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 14 * 16, 10 * 16, map_set, 128, 112, 16, 15, 0));
-
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 5 * 16, 14 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 5 * 16, 14 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 76 * 16, 14 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 76 * 16, 14 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 84 * 16, 8 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 84 * 16, 8 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 86 * 16, 8 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 86 * 16, 8 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 110 * 16, 15 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 110 * 16, 15 * 16, map_set, 128, 112, 16, 15, 0));
-
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 125 * 16, 15 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 125 * 16, 15 * 16, map_set, 128, 112, 16, 15, 0));
-
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 132 * 16, 14 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 132 * 16, 14 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 138 * 16, 8 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 138 * 16, 8 * 16, map_set, 128, 112, 16, 15, 0));
-
-	Dynamic_obj_list.push_back(new Coin("Coin_rot", 185 * 16, 11 * 16, coins_tex, 4, 113, 15, 15, 2));
-	Dynamic_obj_list.push_back(new Q_Block("Q_block", 185 * 16, 11 * 16, map_set, 128, 112, 16, 15, 0));
-
-
-	Dynamic_obj_list.push_back(new Coin("Coin", 9 * 16, 1 * 16, coins_tex, 147, 98, 9, 15, 4)); 
-
-
-	Dynamic_obj_list.push_back(new Flag("Flag", 228.5 * 16, 12.2 * 16, coins_tex, 128, 1, 16, 15, 0)); 
-
+	_world_generation(Dynamic_obj_list, map_set, coins_tex);  // working only with  /FORCE:MULTIPLE
 
 	sf::Clock clock;
 	clock.restart();
@@ -451,7 +362,7 @@ int main()
 		for (auto it = Dynamic_obj_list.begin(); it != Dynamic_obj_list.end();)
 		{
 			
-			if (Super_mario.rect.intersects((*it)->rect) && (*it)->name == "Coin_classic"&& Super_mario.alive)
+			if (Super_mario.rect.intersects((*it)->rect) && (*it)->name == "Coin_classic" && Super_mario.alive)
 			{
 				++Super_mario.coins;
 				Super_mario.points += 200;
@@ -620,11 +531,11 @@ int main()
 
 
 
-		//////////////////////Working wi text, sometime not stable ////////////////
+		//////////////////////Working with text, sometime not stable ////////////////
 
 		std::ostringstream pointsString;
 		pointsString << Super_mario.points;
-		//pointsString << (Super_mario.rect.left/16); // for texting, droing coordinates of Mario
+		//pointsString << (Super_mario.rect.left/16); // for texting, drowing coordinates of Mario
 
 
 		std::stringstream coins;
